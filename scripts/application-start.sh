8860f3a58416187619c024f433af78796810cc2b
Demetrice Ledbetter <demetr1ce@users.noreply.github.com>
2023-08-30 20:54:56 -0700

    pipeline update to start app on deploy

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd /tmp/CodeDeployNodeJS-API
+cd /home/ec2-user/nodejs-aws-api
+node app.js
 
 echo "The ApplicationStart deployment lifecycle event successfully completed." > application-start.txt
