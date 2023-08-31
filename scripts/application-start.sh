e004a9c51c388a13ffd1fdb8f2470be00961f7e5
Demetrice Ledbetter <demetr1ce@users.noreply.github.com>
2023-08-30 23:06:35 -0700

    trigger

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ec2-user/nodejs-aws-api
-node app.js
+sudo cd /home/ec2-user/nodejs-aws-api
+sudo node app.js
 
 echo "The ApplicationStart deployment lifecycle event successfully completed."
