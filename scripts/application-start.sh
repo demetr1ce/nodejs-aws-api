21365e6a1718ecff28b33b4c1b9a446767ce6990
Demetrice Ledbetter <demetr1ce@users.noreply.github.com>
2023-08-31 21:17:23 -0700

    trigger

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -4,6 +4,7 @@ npm config set prefix '~/.npm-global'
 export PATH=~/.npm-global/bin:$PATH
 source ~/.profile
 npm install -g jshint
+cd /home/ec2-user/nodejs-aws-api
 node app.js
 
 echo "The ApplicationStart deployment lifecycle event successfully completed" > application-start.txt
