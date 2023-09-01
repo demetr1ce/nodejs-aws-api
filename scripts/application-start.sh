7aa585fee6a068a9d6768d863221060f027ae3d7
Demetrice Ledbetter <demetr1ce@users.noreply.github.com>
2023-08-31 21:07:43 -0700

    trigger

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-cd ..
-cd /home/ec2-user/nodejs-aws-api
-sudo chown -R $(whoami) ~/nodejs-aws-api
-npm i
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+export PATH=~/.npm-global/bin:$PATH
+source ~/.profile
+npm install -g jshint
 node app.js
 
 echo "The ApplicationStart deployment lifecycle event successfully completed" > application-start.txt
