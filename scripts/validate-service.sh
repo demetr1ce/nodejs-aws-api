41dfa50599a1e9bd4a007cb2704f6b1909f6fd66
Demetrice Ledbetter <demetr1ce@users.noreply.github.com>
2023-09-03 13:32:30 -0700

    trigger

--- scripts/validate-service.sh
+++ scripts/validate-service.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd /home/ec2-user/nodejs-aws-api
 
-accessKeyId=$(aws ssm get-parameters --region us-east-1 --names accessKeyId --with-decryption --query Parameters[0].Value)
-secretAccessKey=$(aws ssm get-parameters --region us-east-1 --names secretAccessKey --with-decryption --query Parameters[0].Value)
-region=$(aws ssm get-parameters --region us-east-1 --names region --query Parameters[0].Value)
-endpoint=$(aws ssm get-parameters --region us-east-1 --names endpoint --query Parameters[0].Value)
+accessKeyId=$(sudo aws ssm get-parameters --region us-east-1 --names accessKeyId --with-decryption --query Parameters[0].Value)
+secretAccessKey=$(sudo aws ssm get-parameters --region us-east-1 --names secretAccessKey --with-decryption --query Parameters[0].Value)
+region=$(sudo aws ssm get-parameters --region us-east-1 --names region --query Parameters[0].Value)
+endpoint=$(sudo aws ssm get-parameters --region us-east-1 --names endpoint --query Parameters[0].Value)
 
 echo "accesKeyId=$accessKeyId\n secretAccessKey=$secretAccessKey\n region=$region\n endpoint=$endpoint" > .env
