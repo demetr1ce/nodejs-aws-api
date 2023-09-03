7c62e3516b08e9e88869f1cd43de780936c0eddf
Demetrice Ledbetter <demetr1ce@users.noreply.github.com>
2023-09-03 13:40:07 -0700

    trigger

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,7 +1,16 @@
 source /home/ec2-user/.bash_profile
+cd /home/ec2-user/nodejs-aws-api
+
+accessKeyId=$(sudo aws ssm get-parameters --region us-east-1 --names accessKeyId --with-decryption --query Parameters[0].Value)
+secretAccessKey=$(sudo aws ssm get-parameters --region us-east-1 --names secretAccessKey --with-decryption --query Parameters[0].Value)
+region=$(sudo aws ssm get-parameters --region us-east-1 --names region --query Parameters[0].Value)
+endpoint=$(sudo aws ssm get-parameters --region us-east-1 --names endpoint --query Parameters[0].Value)
+
+echo -e "accessKeyId=$accessKeyId\nsecretAccessKey=$secretAccessKey\nregion=$region\nendpoint=$endpoint" > .env
+
 sudo chown -R $(whoami) ~/nodejs-aws-api
 cd /home/ec2-user/nodejs-aws-api
 npm i
 node app.js
 
-echo "API successfully started."
+echo "API successfully started." > app-started.txt
--- scripts/validate-service.sh
+++ scripts/validate-service.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/nodejs-aws-api
 
-accessKeyId=$(sudo aws ssm get-parameters --region us-east-1 --names accessKeyId --with-decryption --query Parameters[0].Value)
-secretAccessKey=$(sudo aws ssm get-parameters --region us-east-1 --names secretAccessKey --with-decryption --query Parameters[0].Value)
-region=$(sudo aws ssm get-parameters --region us-east-1 --names region --query Parameters[0].Value)
-endpoint=$(sudo aws ssm get-parameters --region us-east-1 --names endpoint --query Parameters[0].Value)
+echo "The ValidateService deployment lifecycle event successfully completed." > validate-service.txt
 
-echo -e "accessKeyId=$accessKeyId\nsecretAccessKey=$secretAccessKey\nregion=$region\nendpoint=$endpoint" > .env
+unset FOLDER
