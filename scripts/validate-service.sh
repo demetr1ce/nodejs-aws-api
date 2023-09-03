8cabf1946d12c2a8050b58f7c80f8e6b2d03ab56
Demetrice Ledbetter <demetr1ce@users.noreply.github.com>
2023-09-03 13:15:14 -0700

    update to script

--- scripts/validate-service.sh
+++ scripts/validate-service.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd /home/ec2-user/nodejs-aws-api
 
-accessKeyId="accessKeyId=$(aws ssm get-parameters --region us-east-1 --names accessKeyId --with-decryption --query Parameters[0].Value)"
-secretAccessKey="secretAccessKey=$(aws ssm get-parameters --region us-east-1 --names secretAccessKey --with-decryption --query Parameters[0].Value)"
-region="region=$(aws ssm get-parameters --region us-east-1 --names region --query Parameters[0].Value)"
-endpoint="endpoint=$(aws ssm get-parameters --region us-east-1 --names endpoint --query Parameters[0].Value)"
+accessKeyId=$(aws ssm get-parameters --region us-east-1 --names accessKeyId --with-decryption --query Parameters[0].Value)
+secretAccessKey=$(aws ssm get-parameters --region us-east-1 --names secretAccessKey --with-decryption --query Parameters[0].Value)
+region=$(aws ssm get-parameters --region us-east-1 --names region --query Parameters[0].Value)
+endpoint=$(aws ssm get-parameters --region us-east-1 --names endpoint --query Parameters[0].Value)
 
-echo "${accessKeyId}\n${secretAccessKey}\n${region}\n${endpoint}\n" > .env
+echo "accesKeyId=$accessKeyId\n secretAccessKey=$secretAccessKey\n region=$region\n endpoint=$endpoint" > .env
