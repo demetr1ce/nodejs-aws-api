f440b19b4202e847a579e968988b0e232da4b636
Demetrice Ledbetter <demetr1ce@users.noreply.github.com>
2023-09-04 09:45:17 -0700

    script updates

--- scripts/validate-service.sh
+++ scripts/validate-service.sh
@@ -1,6 +1,14 @@
-#!/bin/bash
+source /home/ec2-user/.bash_profile
 cd /home/ec2-user/nodejs-aws-api
 
-echo "The ValidateService deployment lifecycle event successfully completed." > validate-service.txt
+accessKeyId=$(sudo aws ssm get-parameters --region us-east-1 --names accessKeyId --with-decryption --query Parameters[0].Value | tr -d '"') 
+secretAccessKey=$(sudo aws ssm get-parameters --region us-east-1 --names secretAccessKey --with-decryption --query Parameters[0].Value | tr -d '"')
+region=$(sudo aws ssm get-parameters --region us-east-1 --names region --query Parameters[0].Value | tr -d '"')
+endpoint=$(sudo aws ssm get-parameters --region us-east-1 --names endpoint --query Parameters[0].Value | tr -d '"')
 
-unset FOLDER
+echo -e "accessKeyId=$accessKeyId\nsecretAccessKey=$secretAccessKey\nregion=$region\nendpoint=$endpoint" > .env
+
+sudo chown -R $(whoami) ~/nodejs-aws-api
+cd /home/ec2-user/nodejs-aws-api
+npm i
+node app.js
