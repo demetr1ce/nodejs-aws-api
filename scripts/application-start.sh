7d4395927d1e582730de1d9fb732fca7b9516e90
Demetrice Ledbetter <demetr1ce@users.noreply.github.com>
2023-09-04 09:17:05 -0700

    trigger

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,14 +1,14 @@
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/nodejs-aws-api
 
-accessKeyId=$(sudo aws ssm get-parameters --region us-east-1 --names accessKeyId --with-decryption --query Parameters[0].Value)
-secretAccessKey=$(sudo aws ssm get-parameters --region us-east-1 --names secretAccessKey --with-decryption --query Parameters[0].Value)
-region=$(sudo aws ssm get-parameters --region us-east-1 --names region --query Parameters[0].Value)
-endpoint=$(sudo aws ssm get-parameters --region us-east-1 --names endpoint --query Parameters[0].Value)
+accessKeyId=$(sudo aws ssm get-parameters --region us-east-1 --names accessKeyId --with-decryption --query Parameters[0].Value) | tr -d '"'
+secretAccessKey=$(sudo aws ssm get-parameters --region us-east-1 --names secretAccessKey --with-decryption --query Parameters[0].Value) | tr -d '"'
+region=$(sudo aws ssm get-parameters --region us-east-1 --names region --query Parameters[0].Value) | tr -d '"'
+endpoint=$(sudo aws ssm get-parameters --region us-east-1 --names endpoint --query Parameters[0].Value) | tr -d '"'
 
 sudo chown -R $(whoami) ~/nodejs-aws-api
 cd /home/ec2-user/nodejs-aws-api
 npm i
 node app.js
 
-echo -e "accessKeyId=$accessKeyId\nsecretAccessKey=$secretAccessKey\nregion=$region\nendpoint=$endpoint" | tr -d '"' > .env
+echo -e "accessKeyId=$accessKeyId\nsecretAccessKey=$secretAccessKey\nregion=$region\nendpoint=$endpoint" > .env
