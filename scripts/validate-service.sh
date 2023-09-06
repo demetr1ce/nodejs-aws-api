d4ca0000e08f10e647b68153bb929a1457dc7c00
Demetrice Ledbetter <demetr1ce@users.noreply.github.com>
2023-09-05 20:58:24 -0700

    script update

--- scripts/validate-service.sh
+++ scripts/validate-service.sh
@@ -1,6 +1,6 @@
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/nodejs-aws-api
-sudo npm i
-sudo node app.js
+npm i
+node app.js
 
 sudo echo "API successfully started." > application-start.txt
