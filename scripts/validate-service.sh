553130f5fa92a54cbb4ac85fcaa926abebaedaf0
Demetrice Ledbetter <demetr1ce@users.noreply.github.com>
2023-09-05 20:13:53 -0700

    script update

--- scripts/validate-service.sh
+++ scripts/validate-service.sh
@@ -2,7 +2,7 @@ source /home/ec2-user/.bash_profile
 sudo chown -R $(whoami) ~/nodejs-aws-api
 cd /home/ec2-user/nodejs-aws-api
 
-npm i
+sudo npm i
 
 accessKeyId=$(sudo aws ssm get-parameters --region us-east-1 --names accessKeyId --with-decryption --query Parameters[0].Value | tr -d '"') 
 secretAccessKey=$(sudo aws ssm get-parameters --region us-east-1 --names secretAccessKey --with-decryption --query Parameters[0].Value | tr -d '"')
@@ -11,4 +11,4 @@ endpoint=$(sudo aws ssm get-parameters --region us-east-1 --names endpoint --que
 
 echo -e "accessKeyId=$accessKeyId\nsecretAccessKey=$secretAccessKey\nregion=$region\nendpoint=$endpoint" > .env
 
-node app.js
+sudo node app.js
