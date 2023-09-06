c493cfd4673e3ca568ca054fecde390da917c346
Demetrice Ledbetter <demetr1ce@users.noreply.github.com>
2023-09-06 01:08:12 -0700

    added winston

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const server = require(`./server`);
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [new winston.transports.Console()],
+  });
 
 require(`dotenv`).config();
 server.createServer();
