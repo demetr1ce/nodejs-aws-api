f18ac5ffb04aed3378cf4ed3747428cf51d736dd
Demetrice Ledbetter <demetr1ce@users.noreply.github.com>
2023-09-05 20:33:25 -0700

    script updates

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ec2-user/nodejs-aws-api
 
-echo "The AfterInstall deployment lifecycle event successfully completed."
+echo "The AfterInstall deployment lifecycle event successfully completed." > after-install.txt
