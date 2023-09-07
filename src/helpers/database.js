f19697412c1e37d13599ab913e2e23342a472a02
Demetrice Ledbetter <demetr1ce@users.noreply.github.com>
2023-09-06 20:58:35 -0700

    update

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -1,17 +1,10 @@
 var AWS = require('aws-sdk');
-var https = require('node:https');
 
 AWS.config.update({
     accessKeyId: process.env.accessKeyId,
     secretAccessKey: process.env.secretAccessKey,
     region: process.env.region,
     endpoint: process.env.endpoint,
-});
-
-AWS.config.httpOptions['agent'] = new https.Agent({
-    keepAlive: true,
-    ciphers: 'ALL',
-    secureProtocol: 'TLSv1_method'
 });
 
 process.on('uncaughtException', function (err) {
