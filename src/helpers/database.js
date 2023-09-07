8bc67290de85dfd876e2dfc3458f8cc298edadd8
Demetrice Ledbetter <demetr1ce@users.noreply.github.com>
2023-09-06 20:53:09 -0700

    debugging

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -1,4 +1,5 @@
 var AWS = require('aws-sdk');
+var https = require('node:https');
 
 AWS.config.update({
     accessKeyId: process.env.accessKeyId,
@@ -11,6 +12,10 @@ AWS.config.httpOptions['agent'] = new https.Agent({
     keepAlive: true,
     ciphers: 'ALL',
     secureProtocol: 'TLSv1_method'
+});
+
+process.on('uncaughtException', function (err) {
+    logger.log('error','UNCAUGHT EXCEPTION - keeping process alive:',  err);
 });
 
 const db = new AWS.DynamoDB.DocumentClient({convertEmptyValues: true});
